a5091478c72ce0720fbb5806688ef528bf2090fd
Gura Ilya <igura@t1-consulting.ru>
2023-07-26 18:29:25 +0300

    update

--- middleware/error-middleware.js
+++ middleware/error-middleware.js
@@ -1,7 +1,6 @@
 const ApiError = require('./exceptions/api-error')
 
 function errorMiddleware(err, req, res, next) {
-    console.log(err);
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message, errors: err.errors});
     }
